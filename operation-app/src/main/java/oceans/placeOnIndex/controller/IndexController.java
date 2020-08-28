df59577e21a2bca0d1af99bb4db447c7472f19e4
liaopan <liaopan@bmsoft.com.cn>
2020-08-28 15:48:12 +0800

    跳转页面需要用controller，而不是RestController

--- operation-app/src/main/java/oceans/placeOnIndex/controller/IndexController.java
+++ operation-app/src/main/java/oceans/placeOnIndex/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//跳转页面需要用controller，而不是RestController
 @Controller
 @RequestMapping("/index")
 public class IndexController {
